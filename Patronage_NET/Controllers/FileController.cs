a6c500e769e430ff82163898c8a87dd5b35f3716
Michal Tymejczyk <michal.tymejczyk@gmail.com>
2019-12-29 00:38:17 +0100

    POST and PUT

---
+++ Patronage_NET/Controllers/FileController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Patronage_NET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly string FullPath;
+        private readonly string FolderName;
+        private readonly int FileSizeCap;
+        private readonly int ContentCap;
+
+        // Controller
+        public FileController()
+        {
+            var DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            FolderName = "TaskOne";
+            FullPath = System.IO.Path.Combine(DesktopPath, FolderName);
+
+            FileSizeCap = 255;
+            ContentCap = 50;
+        }
+
+        private bool isContentValid(string content)
+        {
+            return content.Length <= ContentCap;
+        }
+
+        private void nextFileName(ref string path, string name, string content)
+        {
+            var fileLength = new System.IO.FileInfo(path).Length;
+            var contentLength = content.Length;
+
+            if ((fileLength + contentLength) > FileSizeCap)
+            {
+                int i = 1;
+                while (true)
+                {
+                    path = System.IO.Path.Combine(FullPath, name + $"-{i}");
+
+                    if (System.IO.File.Exists(path))
+                    {
+                        fileLength = new System.IO.FileInfo(path).Length;
+
+                        if ((fileLength + contentLength) <= FileSizeCap)
+                        {
+                            break;
+                        }
+                    }
+                    else if (!System.IO.File.Exists(path))
+                    {
+                        System.IO.File.Create(path);
+                        break;
+                    }
+
+                    i++;
+                }
+            }
+        }
+
+        [HttpGet]
+        public async Task<string/*HttpResponseMessage*/> GetAll(string name)
+        {
+
+
+            return "";
+        }
+
+        [HttpPost]
+        public async Task<string> Post([FromBody]Myfile myfile)
+        {
+            (var name, var content) = myfile.Deconstruct();
+
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+            if (!System.IO.Directory.Exists(FullPath))
+            {
+                System.IO.Directory.CreateDirectory(FullPath);
+            }
+
+            if (!isContentValid(content))
+            {
+                return "CONTENT OVERFLOW";
+            }
+
+            var filepath = System.IO.Path.Combine(FullPath, name);
+
+            await System.IO.File.WriteAllTextAsync(filepath, content);
+
+            Response.StatusCode = (int)HttpStatusCode.OK;
+            return $"Created {filepath}";
+        }
+
+        [HttpPut]
+        public async Task<string> Put([FromBody]Myfile myfile)
+        {
+            (var name, var content) = myfile.Deconstruct();
+
+            var filepath = System.IO.Path.Combine(FullPath, name);
+
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+            if (!System.IO.File.Exists(filepath))
+            {
+                return $"File doesn't exist";
+            }
+
+            if (!isContentValid(content))
+            {
+                return $"CONTENT OVERFLOW";
+            }
+
+            nextFileName(ref filepath, name, content);
+
+            await System.IO.File.AppendAllTextAsync(filepath, Environment.NewLine + content);
+
+            Response.StatusCode = (int)HttpStatusCode.OK;
+            return $"Updated {filepath}";
+        }
+    }
+}
---
+++ Patronage_NET/Models/Myfile.cs
@@ -0,0 +1,11 @@
+﻿namespace Patronage_NET
+{
+    public class Myfile
+    {
+        public (string name, string content) Deconstruct() => (this.name, this.content);
+
+        public string name { get; set; }
+        public string content { get; set; }
+
+    }
+}
